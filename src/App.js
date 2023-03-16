0d07a5ef6cb1c521287e0863726ed538337e6251
Titus Ndondo <Titus.Ndondo@investec.co.za>
2023-03-16 21:48:49 +0200

    message

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
-    { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
-    { title: 'Toilet Paper', amount: 294.67, date: new Date(2021, 2, 28) },
-    { title: 'New Desk (Wooden)', amount: 294.67, date: new Date(2021, 2, 28) },
-    { title: 'New TV', amount: 294.67, date: new Date(2021, 2, 28) }
+    { id: '1', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
+    { id: '2', title: 'Toilet Paper', amount: 294.67, date: new Date(2021, 2, 28) },
+    { id: '3', title: 'New Desk (Wooden)', amount: 294.67, date: new Date(2021, 2, 28) },
+    { id: '4', title: 'New TV', amount: 294.67, date: new Date(2021, 2, 28) }
   ]
 
-  return (
+  const addExpenseHandler = (expense) => {
+    console.log(expense)
+  }
+
+  return (<>
+      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
       <Expenses expenses={ expenses }/>
+    </>
   );
 }
 
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
@@ -5,13 +6,21 @@ import './ExpenseItem.css';
 const ExpenseItem = ({ expense }) => {
     console.log('EXPENSES', expense);
 
+    const [title, setTitle] = useState(expense.title);
+
+    const clickHandler = () => {
+        console.log('Clicked!!!')
+        setTitle('updated');
+    }
+
     return (
         <Card className='expense-item'>
             <ExpenseDate date={expense.date}/>
             <div className='expense-item__description'>
-                <h2>{expense.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${expense.amount}</div>
             </div>
+            <button onClick={ clickHandler } >Change title</button>
         </Card>
     );
 }
